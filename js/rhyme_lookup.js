1ab361ae608f592dd5759730407428d2d3f034b7
Owen Jow <owenjow@berkeley.edu>
2015-10-11 04:08:22 -0700

    Exclusivized search to END of pronunciation

--- js/rhyme_lookup.js
+++ js/rhyme_lookup.js
@@ -61134,7 +61134,25 @@ for (var mobyword in mobypron) {
 var lookup = function(word) {
     if (mobypron.hasOwnProperty(word)) {
         var pronFields = mobypron[word].split(/\/+/);
-        return mobytrie.autoComplete(pronFields);
+        if (pronFields.length > 2) {
+            pronFields.splice(0, pronFields.length - 2);
+        }
+        
+        var results = mobytrie.autoComplete(pronFields);
+        if (results.length == 0) { return []; }
+        
+        /* // Get the shortest word from results
+        var shortestWord = results[0];
+        var longestWord = null;
+        for (var i = 1; i < results.length; i++) {
+            var r = results[i];
+            if (r.length < shortestWord.length || Math.random() > 0.42) {
+                shortestWord = r;
+            }
+        } */
+        
+        // Return a random word from results
+        return [results[Math.floor(Math.random() * results.length)]];
     } else {
         return [];
     }
